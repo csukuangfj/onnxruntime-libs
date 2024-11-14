ff98449b4916d56b7153d85ddd1cace1357801a0
Fangjun Kuang <csukuangfj@gmail.com>
2024-11-14 18:11:00 +0800

    small fixes

--- .github/scripts/patchelf.sh
+++ .github/scripts/patchelf.sh
@@ -1,35 +1,31 @@
 #!/usr/bin/env bash
 
+set -ex
+
 version=$ONNXRUNTIME_VERSION
 
-files=(
-onnxruntime-linux-aarch64-glibc2_17-Debug-$version
-onnxruntime-linux-aarch64-glibc2_17-MinSizeRel-$version
-onnxruntime-linux-aarch64-glibc2_17-Release-$version
-onnxruntime-linux-aarch64-glibc2_17-RelWithDebInfo-$version
-onnxruntime-linux-arm-$version
-onnxruntime-linux-x64-glibc2_17-Debug-$version
-onnxruntime-linux-x64-glibc2_17-MinSizeRel-$version
-onnxruntime-linux-x64-glibc2_17-Release-$version
-onnxruntime-linux-x64-glibc2_17-RelWithDebInfo-$version
-)
+f=onnxruntime-linux-x64-gpu-$ONNXRUNTIME_VERSION
+curl -SL -O https://github.com/microsoft/onnxruntime/releases/download/v$ONNXRUNTIME_VERSION/$f.tgz
+tar xvf $f.tgz
+rm $f.tgz
 
-for f in ${files[@]}; do
-  curl -SL -O https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.17.1/$f.zip
-  unzip $f.zip
-  rm $f.zip
-  mv $f $f-patched
-  f=$f-patched
-  pushd $f/lib
-  rm -v libonnxruntime.so.$version
+mv $f $f-patched
+f=$f-patched
+pushd $f/lib
+ls -lh
+readelf -d libonnxruntime.so.$version
 
-  readelf -d libonnxruntime.so
-  echo "----"
-  patchelf --set-soname libonnxruntime.so ./libonnxruntime.so
-  echo "----"
-  readelf -d libonnxruntime.so
-  popd
-  zip -r $f.zip $f
-  rm -rf $f
-  mv $f.zip ../..
-done
+rm -v libonnxruntime.so
+rm -v libonnxruntime.so.1
+mv -v libonnxruntime.so.$version libonnxruntime.so
+
+readelf -d libonnxruntime.so
+
+echo "----"
+patchelf --set-soname libonnxruntime.so ./libonnxruntime.so
+echo "----"
+readelf -d libonnxruntime.so
+popd
+zip -r $f.zip $f
+rm -rf $f
+mv $f.zip ../..
