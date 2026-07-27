b9575d3787bb28919756d25dbb313cebe64ff417
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-27 15:44:02 +0800

    Fix optional unwrapping: GetVersionString() returns Optional<UnsafePointer>

--- spm-examples/iOS/Sources/main.swift
+++ spm-examples/iOS/Sources/main.swift
@@ -1,8 +1,8 @@
 import onnxruntime
 
 let base = OrtGetApiBase()!
-let getVersionString = base.pointee.GetVersionString!
-let version = String(cString: getVersionString())
+let cStr = base.pointee.GetVersionString!()
+let version = String(cString: cStr!)
 let apiVersion = ORT_API_VERSION
 
 print("ONNX Runtime version: \(version)")
