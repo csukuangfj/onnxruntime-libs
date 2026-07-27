6e27be138be610c9dbdfed137213c64d78ea9885
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-27 15:00:42 +0800

    Make test-spm CI more verbose: show dependency tree and full build output

--- spm-examples/VersionDemo/Package.swift
+++ spm-examples/VersionDemo/Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "VersionDemo",
-    platforms: [.macOS(.v10_15)],
-    dependencies: [
-        .package(url: "https://github.com/csukuangfj/onnxruntime-libs", branch: "release-v1.27.1"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "VersionDemo",
-            dependencies: [
-                .product(name: "OnnxRuntime", package: "onnxruntime-libs"),
-            ]
-        ),
-    ]
+  name: "VersionDemo",
+  platforms: [.macOS(.v10_15)],
+  dependencies: [
+    .package(url: "https://github.com/csukuangfj/onnxruntime-libs", branch: "release-v1.27.1")
+  ],
+  targets: [
+    .executableTarget(
+      name: "VersionDemo",
+      dependencies: [
+        .product(name: "OnnxRuntime", package: "onnxruntime-libs")
+      ]
+    )
+  ]
 )
