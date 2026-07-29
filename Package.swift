f6b9fbb2295e74ce3aecc0fec48047dfc30d4ca9
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-29 09:53:56 +0800

    change spm name

--- Package.swift
+++ Package.swift
@@ -15,24 +15,24 @@ let package = Package(
             targets: ["OnnxRuntime"]
         ),
         // Per-platform binary targets for direct use
-        .library(name: "onnxruntime-macos", targets: ["onnxruntime-macos"]),
-        .library(name: "onnxruntime-ios", targets: ["onnxruntime-ios"]),
-        .library(name: "onnxruntime-visionos", targets: ["onnxruntime-visionos"]),
+        .library(name: "onnxruntime-macos", targets: ["OnnxruntimeMacOS"]),
+        .library(name: "onnxruntime-ios", targets: ["OnnxruntimeIOS"]),
+        .library(name: "onnxruntime-visionos", targets: ["OnnxruntimeVisionOS"]),
     ],
     targets: [
         // Binary targets for pre-built xcframeworks
         .binaryTarget(
-            name: "onnxruntime-macos",
+            name: "OnnxruntimeMacOS",
             url: "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.27.1/onnxruntime-macos-static-xcframework-1.27.1.xcframework.zip",
             checksum: "89769c25a63985e2ab7a12e72215c173c5078e49dc4a2273cb84b75e587d7b96"
         ),
         .binaryTarget(
-            name: "onnxruntime-ios",
+            name: "OnnxruntimeIOS",
             url: "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.27.1/onnxruntime-ios-static-xcframework-1.27.1.xcframework.zip",
             checksum: "985deaff345c7bcfbe4979b2daeec09d7a745b1e9cb73f37f4077364eb578e62"
         ),
         .binaryTarget(
-            name: "onnxruntime-visionos",
+            name: "OnnxruntimeVisionOS",
             url: "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.27.1/onnxruntime-visionos-static-xcframework-1.27.1.xcframework.zip",
             checksum: "206cdc2f23a1679aa3b3a91a74a866fad56a7ea50709fe5d4a67fb9b868c6234"
         ),
@@ -41,9 +41,9 @@ let package = Package(
         .target(
             name: "OnnxRuntime",
             dependencies: [
-                "onnxruntime-macos",
-                "onnxruntime-ios",
-                "onnxruntime-visionos",
+                "OnnxruntimeMacOS",
+                "OnnxruntimeIOS",
+                "OnnxruntimeVisionOS",
             ],
             linkerSettings: [
                 .linkedFramework("CoreFoundation"),
