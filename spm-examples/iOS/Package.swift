c60f94f4a2e871683a85e62e5b8f2005ac1e8fd1
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-30 15:59:22 +0800

    switch to master

--- spm-examples/iOS/Package.swift
+++ spm-examples/iOS/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "VersionDemo",
     platforms: [.iOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/csukuangfj/onnxruntime-libs", branch: "fix-macos"),
+        .package(url: "https://github.com/csukuangfj/onnxruntime-libs", branch: "master"),
     ],
     targets: [
         .executableTarget(
